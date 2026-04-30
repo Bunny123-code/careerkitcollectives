a8c848d1f9bafb0ad825a00bc1d2b2c719fb7bb3
Bunny123-code <hashirqadeer12@gmail.com>
2026-04-30 08:05:39 +0500

    Fix GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/careerkitcollectives">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/products" element={<Index />} />
